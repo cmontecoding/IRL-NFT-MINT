bebaad23ae8003b1773249ad95a3343a48afda0d
Andrew Chiaramonte <andrewchiaramonte4104@gmail.com>
2024-06-25 21:43:48 -0400

    add nft claim

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,98 +1,88 @@
-import Image from "next/image";
-import { ConnectButton } from "thirdweb/react";
-import thirdwebIcon from "@public/thirdweb.svg";
+"use client";
+import { ConnectButton, TransactionButton, useReadContract } from "thirdweb/react";
 import { client } from "./client";
+import { createWallet } from "thirdweb/wallets";
+import { defineChain, getContract } from "thirdweb";
+import { baseSepolia } from "thirdweb/chains";
+import { getNFT, balanceOf, claimTo } from "thirdweb/extensions/erc1155"
+import { MediaRenderer, useActiveAccount } from "thirdweb/react";
 
 export default function Home() {
+  const account = useActiveAccount();
+  const wallets = [
+    createWallet("com.coinbase.wallet")
+  ]; 
+  const contract = getContract({ 
+    client: client,
+    chain: defineChain( baseSepolia ),
+    address: "0xa6FFd2FCF3967dA69e758e95FF019A547EF42607"
+  });
+
+  const { data:nft, isLoading: nftIsLoading } = useReadContract(
+    getNFT,
+    {
+      contract: contract,
+      tokenId: 0n
+    }
+  )
+
+  const { data: ownedNFTs } = useReadContract(
+    balanceOf,
+    {
+      contract: contract,
+      owner: account?.address || "",
+      tokenId: 0n
+    }
+  )
+
   return (
     <main className="p-4 pb-10 min-h-[100vh] flex items-center justify-center container max-w-screen-lg mx-auto">
-      <div className="py-20">
-        <Header />
-
-        <div className="flex justify-center mb-20">
-          <ConnectButton
-            client={client}
-            appMetadata={{
-              name: "Example App",
-              url: "https://example.com",
-            }}
-          />
+      <div className="flex flex-col items-center py-20">
+        <ConnectButton
+          client={client}
+          wallets={wallets} 
+          chain={defineChain( baseSepolia )}
+          connectButton = {{
+            label: "Connect with Coinbase Smart Wallet"
+          }}
+        ></ConnectButton>
+        <div className="flex flex-col my-8">
+          {nftIsLoading ? (
+            <p>Loading...</p>
+          ) : (
+            <>
+            {nft && (
+              <MediaRenderer
+              client={client}
+              src={nft.metadata.image}
+              />
+            )}
+            {account ? (
+              <>
+              <p className="text-center mt-8">
+                You own {ownedNFTs?.toString()} NFTs
+              </p>
+              <TransactionButton
+                transaction={() =>
+                  claimTo({
+                    contract: contract,
+                    to: account.address,
+                    tokenId: 0n,
+                    quantity: 1n
+                  })
+                }
+                onTransactionConfirmed={async () => {
+                  alert("NFT claimed!");
+                }}
+              >Claim</TransactionButton>
+              </>
+            ) : (
+              <p>Connect Coinbase Smart Wallet to claim NFT</p>
+            )}
+            </>
+          )}
         </div>
-
-        <ThirdwebResources />
       </div>
     </main>
-  );
-}
-
-function Header() {
-  return (
-    <header className="flex flex-col items-center mb-20 md:mb-20">
-      <Image
-        src={thirdwebIcon}
-        alt=""
-        className="size-[150px] md:size-[150px]"
-        style={{
-          filter: "drop-shadow(0px 0px 24px #a726a9a8)",
-        }}
-      />
-
-      <h1 className="text-2xl md:text-6xl font-semibold md:font-bold tracking-tighter mb-6 text-zinc-100">
-        thirdweb SDK
-        <span className="text-zinc-300 inline-block mx-1"> + </span>
-        <span className="inline-block -skew-x-6 text-blue-500"> Next.js </span>
-      </h1>
-
-      <p className="text-zinc-300 text-base">
-        Read the{" "}
-        <code className="bg-zinc-800 text-zinc-300 px-2 rounded py-1 text-sm mx-1">
-          README.md
-        </code>{" "}
-        file to get started.
-      </p>
-    </header>
-  );
-}
-
-function ThirdwebResources() {
-  return (
-    <div className="grid gap-4 lg:grid-cols-3 justify-center">
-      <ArticleCard
-        title="thirdweb SDK Docs"
-        href="https://portal.thirdweb.com/typescript/v5"
-        description="thirdweb TypeScript SDK documentation"
-      />
-
-      <ArticleCard
-        title="Components and Hooks"
-        href="https://portal.thirdweb.com/typescript/v5/react"
-        description="Learn about the thirdweb React components and hooks in thirdweb SDK"
-      />
-
-      <ArticleCard
-        title="thirdweb Dashboard"
-        href="https://thirdweb.com/dashboard"
-        description="Deploy, configure, and manage your smart contracts from the dashboard."
-      />
-    </div>
-  );
-}
-
-function ArticleCard(props: {
-  title: string;
-  href: string;
-  description: string;
-}) {
-  return (
-    <a
-      href={props.href + "?utm_source=next-template"}
-      target="_blank"
-      className="flex flex-col border border-zinc-800 p-4 rounded-lg hover:bg-zinc-900 transition-colors hover:border-zinc-700"
-    >
-      <article>
-        <h2 className="text-lg font-semibold mb-2">{props.title}</h2>
-        <p className="text-sm text-zinc-400">{props.description}</p>
-      </article>
-    </a>
   );
 }
